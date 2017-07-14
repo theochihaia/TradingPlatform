368ef64a51f3e825b8aaa3b505bc593e30a0178b
theo chihaia <theochihaia@gmail.com>
2017-07-13 22:30:30 -0700

    Add Folders

---
+++ TradingResearchAPI/TradingResearchAPI/Database/SQL/Procedures.sql
@@ -0,0 +1,22 @@
+﻿-- Stored Procs
+
+/*
+	Stock
+		GetStockDetail
+		InsertStock
+		UpdateStock
+*/
+
+/*
+	TradingSession
+		GetTradingSession(Symbol, StartDate, EndDate)
+		InsertTradingSession(Symbol, TradingDate, Open Price, Close Price, High Price, Low Price, Volume)
+		DeleteTradingSession(Symbol, StartDate, EndDate)
+*/
+
+/*
+	Trade
+		GetTrades(Symbol, StartDate, EndDate)
+		InsertTrade(Symbol, TradingDate, TradePrice, TradeAmount, NumberOfShares)
+		DeleteTrade(Symbol, StartDate, EndDate)
+*/
---
+++ TradingResearchAPI/TradingResearchAPI/Database/SQL/Tables.sql
@@ -0,0 +1,19 @@
+﻿-- Tables 
+
+/*
+	Stock
+		ID, Symbol, Name, DateInserted, IsActive
+*/
+
+
+/* 
+	Trading Session
+		ID, SymbolID, TradingDate, Open Price, Close Price, High Price, Low Price, Volume
+*/
+
+
+/*
+	Trade
+		ID, SymbolID, TradingDate, TradePrice, TradeAmount, NumberOfShares
+*/
+
